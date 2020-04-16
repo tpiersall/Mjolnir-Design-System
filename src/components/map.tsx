0768bc0c6c6ddc7650367385bbdf8ffadc1439a8
toddpiersall <tpiersall@buxtonco.com>
2020-04-16 11:51:03 -0500

    updated the map component

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,86 +1,73 @@
 import React, { useState, useRef, useEffect } from 'react';
 import useSwr from 'swr'
 import { CheckBox } from './checkbox'
-import ReactMapGL, { Marker, NavigationControl, GeolocateControl, Layer, Source } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import styled from 'styled-components'
+import mapboxgl from "mapbox-gl"
+import geoViewport from '@mapbox/geo-viewport'
 
-// const fetcher = (...args) => fetch(...args).then(response => response.json())
 
-const INITIAL_VIEWPORT = {
-    latitude: 33.167,
-    longitude: -98.840,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    zoom: 15,
-}
+export const Map = ({
+    width,
+    height,
+    zoom,
+    center,
+    bounds,
+    padding,
+    styles,
+    sources,
+    layers,
+    minZoom,
+    maxZoom
+}) => {
 
-export const Map = () => {
 
-    const [viewport, setViewport] = useState(INITIAL_VIEWPORT)
-    const [userPosition, setUserPosition] = useState(null)
-    const [layerToggle, setLayerToggle] = useState(false)
-    const [color, setColor] = useState('#000')
-
+    const mapNode = useRef(null)
+    const mapRef = useRef(null)
 
     useEffect(() => {
-        function handleResize() {
-            setViewport({ ...viewport, width: window.innerWidth, height: window.innerHeight })
+        let mapCenter = center
+        let mapZoom = zoom
+
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoidHBpZXJzYSIsImEiOiJjaWp1ZzVjNGkwZmU3dTdtOHJmYTZncmJoIn0.8-K2nTXJ7lr4afCulpm39w'
+
+        const map = new mapboxgl.Map({
+            container: mapNode.current,
+            style: "mapbox://styles/tpiersa/ck726obkv0f7x1io5uba4cggs",
+            center: center,
+            zoom: zoom
+        })
+        mapRef.current = map
+        // window.map = map // for easier debugging and querying via console
+
+        map.addControl(new mapboxgl.NavigationControl(), 'top-right')
+
+
+        map.on('load', () => {
+            console.log('map onload')
+            // add sources
+            Object.entries(sources).forEach(([id, source]) => {
+                map.addSource(id, source)
+            })
+
+            // add layers
+            layers.forEach(layer => {
+                map.addLayer(layer)
+            })
+        })
+
+        return () => {
+            map.remove()
         }
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    })
 
-
-
-    const mapRef = useRef();
-
-    // const url = "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
-    // const { data, error } = useSwr(url, fetcher)
-    // const crimes = data && !error ? data.slice(0, 1000) : [];
-
-    const handleColor = (e) => {
-        setColor(e.target.value)
-    }
-    const toggleLayer = () => {
-        setLayerToggle(!layerToggle)
-    }
-
+    }, [])
 
 
     return (
 
-        <ReactMapGL
-            width="100%"
-            height="100%"
-            {...viewport}
-            mapboxApiAccessToken='pk.eyJ1IjoidHBpZXJzYSIsImEiOiJjaWp1ZzVjNGkwZmU3dTdtOHJmYTZncmJoIn0.8-K2nTXJ7lr4afCulpm39w'
-            onViewportChange={(newViewport) => setViewport(newViewport)}
-        >
-            <div style={{ position: 'absolute', left: 16, top: 16 }}>
+        <div ref={mapNode} style={{ width: '100%', height: '100%' }} />
 
-                <NavigationControl />
-            </div>
-            <MapLegend>
-                <CheckBox labelPosition="left" onClick={toggleLayer}>Contours</CheckBox>
-                <input type="color" onChange={handleColor} />
-            </MapLegend>
-            <Source id="contours" type="vector" url="mapbox://mapbox.mapbox-terrain-v2" />
-            <Layer
-                id="contours"
-                type="line"
-                source="contours"
-                source-layer="contour"
-                paint={{
-                    'line-color': `${color}`,
-                    'line-width': 1
-                }}
-                layout={{
-                    'visibility': `${layerToggle ? 'visible' : 'none'}`
-                }}
-            />
-
-        </ReactMapGL>
     )
 }
 
